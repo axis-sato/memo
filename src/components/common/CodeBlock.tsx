f03684b68ce36e82dfd35f3d88efbc74be57d7bf
sato <sato_m@mediaxis.jp>
2020-11-08 20:45:14 +0900

    Mark Downで"```"を入力してもエラーにならないように修正

--- src/components/common/CodeBlock.tsx
+++ src/components/common/CodeBlock.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { coy } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { base16AteliersulphurpoolLight } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 const CodeBlock: React.FC<{
   value: string;
   language?: string;
 }> = ({ language, value }) => {
+  if (value === undefined) return null;
   return (
-    <SyntaxHighlighter language={language} style={coy}>
+    <SyntaxHighlighter
+      language={language}
+      style={base16AteliersulphurpoolLight}
+    >
       {value}
     </SyntaxHighlighter>
   );
