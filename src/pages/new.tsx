c3b3aba5e64bc92505ef3ba17b69e6421b21169c
sato <sato_m@mediaxis.jp>
2020-11-09 23:38:28 +0900

    メモを新規登録後、ホーム画面に遷移する

--- src/pages/new.tsx
+++ src/pages/new.tsx
@@ -7,6 +7,8 @@ import BodyInput from "../components/page/new/BodyInput";
 import TagInput from "../components/page/new/TagInput";
 import TitleInput from "../components/page/new/TitleInput";
 import { useCreateMemo } from "../components/page/new/useCreateMemo";
+import { useRouter } from "next/router";
+import { routes } from "../utils/routes";
 
 interface Input {
   title: string;
@@ -17,10 +19,12 @@ interface Input {
 const New: NextPage = () => {
   const methods = useForm<Input>();
   const { createMemo } = useCreateMemo();
+  const router = useRouter();
 
   const onSubmit = (data: Input) => {
     const tags = data.tags.split(",");
     createMemo({ ...data, tags: tags });
+    router.push(routes.home);
   };
 
   return (
