2b43f50bb268bddc1edd6ae4682295cd2321f15e
sato <sato_m@mediaxis.jp>
2020-11-09 23:35:07 +0900

    Add routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,13 @@ import Link from "next/link";
 import Layout from "../components/layout/Layout";
 import { useFetchMemos } from "../components/page/home/fetchMemos";
 import Memo from "../components/page/home/Memo";
+import { routes } from "../utils/routes";
 
 const Home: NextPage = () => {
   const { memos } = useFetchMemos();
   return (
     <Layout>
-      <Link href="/new">new</Link>
+      <Link href={routes.new}>new</Link>
       {memos.map((memo) => (
         <Memo key={memo.id} memo={memo} />
       ))}
