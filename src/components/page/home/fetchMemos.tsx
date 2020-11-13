4d60785ae619b9a5357ac72e718a78f8145db27e
sato <sato_m@mediaxis.jp>
2020-11-13 20:26:30 +0900

    メモを特定の数ずつ取れるようにした

--- src/components/page/home/fetchMemos.tsx
+++ src/components/page/home/fetchMemos.tsx
@@ -1,5 +1,6 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { db } from "../../../lib/firebase";
+import firebase from "firebase/app";
 
 export type Memo = {
   id: string;
@@ -8,29 +9,41 @@ export type Memo = {
   tags: string[];
 };
 
+const limit = 2;
+
 export const useFetchMemos = () => {
   const [memos, setMemos] = useState<Memo[]>([]);
+  const lastMemo = useRef<firebase.firestore.QueryDocumentSnapshot<
+    firebase.firestore.DocumentData
+  > | null>(null);
+
   const fetchMemos = useCallback(async () => {
     // TODO: エラーハンドリング
-    const memosSnapshot = await db
-      .collection("memos")
-      .orderBy("createdAt", "desc")
-      .get();
-    const memos: Memo[] = [];
+    const query = lastMemo.current
+      ? db
+          .collection("memos")
+          .orderBy("createdAt", "desc")
+          .startAfter(lastMemo.current)
+          .limit(limit)
+      : db.collection("memos").orderBy("createdAt", "desc").limit(limit);
+
+    const memosSnapshot = await query.get();
+    lastMemo.current = memosSnapshot.docs[memosSnapshot.docs.length - 1];
+    const newMemos: Memo[] = [];
     memosSnapshot.forEach((memo) => {
-      memos.push({
+      newMemos.push({
         id: memo.id,
         title: memo.data().title,
         body: memo.data().body,
         tags: memo.data().tags,
       });
     });
-    setMemos(memos);
+    setMemos((oldMemos) => [...oldMemos, ...newMemos]);
   }, []);
 
   useEffect(() => {
     fetchMemos();
   }, [fetchMemos]);
 
-  return { memos };
+  return { memos, fetchMemos };
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,15 @@ import { useFetchMemos } from "../components/page/home/fetchMemos";
 import Memo from "../components/page/home/Memo";
 
 const Home: NextPage = () => {
-  const { memos } = useFetchMemos();
+  const { memos, fetchMemos } = useFetchMemos();
   return (
     <Layout>
       <CreateMemoButton />
       {memos.map((memo) => (
         <Memo key={memo.id} memo={memo} />
       ))}
+
+      <button onClick={() => fetchMemos()}>more</button>
     </Layout>
   );
 };
