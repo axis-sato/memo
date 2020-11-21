d13ece2f6ad5d94baafb49dd4d636f46e53d1254
sato <sato_m@mediaxis.jp>
2020-11-21 22:59:08 +0900

    pタグの中にh1タグなどが存在すると警告が吐かれるので、MarkDownコンポーネントをpタグで括らないようにした

--- src/features/showMemoList/Memo.tsx
+++ src/features/showMemoList/Memo.tsx
@@ -7,16 +7,16 @@ const Memo: React.FC<{ memo: MemoType }> = ({ memo }) => {
   return (
     <div key={memo.id} className="my-5">
       <Title>{memo.title}</Title>
-      <p className="my-10">
+      <div className="my-10">
         <MarkDown>{memo.body}</MarkDown>{" "}
-      </p>
-      <p className="mb-4 flex flex-wrap">
+      </div>
+      <div className="mb-4 flex flex-wrap">
         {memo.tags.map((tag) => (
           <Tag key={tag} className="mr-1 mb-1">
             {tag}
           </Tag>
         ))}
-      </p>
+      </div>
       <hr />
     </div>
   );
